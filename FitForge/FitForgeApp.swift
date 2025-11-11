78394da35254570e03b70afba4a1a982c7042f24
sutej <sutejym122@gmail.com>
2025-11-11 18:28:20 -0500

    Initial Commit

---
+++ FitForge/FitForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitForgeApp.swift
+//  FitForge
+//
+//  Created by Sutej  Ym  on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FitForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitForgeTests/FitForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitForgeTests.swift
+//  FitForgeTests
+//
+//  Created by Sutej  Ym  on 11/11/25.
+//
+
+import Testing
+@testable import FitForge
+
+struct FitForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
